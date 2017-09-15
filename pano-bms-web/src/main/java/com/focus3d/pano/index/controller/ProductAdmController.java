e3e5bc2495813454b82fa321142d1e779a948dcb
focus3d-fc <lihaijun@3d-focus.com>
2017-09-15 15:34:43 +0800

    update

--- pano-bms-web/src/main/java/com/focus3d/pano/index/controller/ProductAdmController.java
+++ pano-bms-web/src/main/java/com/focus3d/pano/index/controller/ProductAdmController.java
@@ -31,140 +31,141 @@ import com.focustech.cief.filemanage.client.constant.FileAttributeEnum;
 import com.focustech.common.utils.EncryptUtil;
 import com.focustech.common.utils.JsonUtils;
 
-
 /**
  * 
  * @author jing
- *
+ * 
  */
 @Controller
-@RequestMapping(value ="/productadm")
-public class ProductAdmController extends BaseController{
-	
+@RequestMapping(value = "/productadm")
+public class ProductAdmController extends BaseController {
+
 	@Autowired
 	private IProductAdmService productAdmService;
 	@Autowired
-	private IFileReadClient fileReadClient;//读取文件接口
+	private IFileReadClient fileReadClient;// 读取文件接口
 
-	//查询列表
+	// 查询列表
 	@RequestMapping("/listproduct")
-	public String listproduct(HttpSession session, ModelMap model,String proid,String productName,String styleSn,String funcSn
-			,Integer pageNum,Integer pageSize,String ifscfy){
-		Map<String,Object> paramMap=new HashMap<String,Object>();
-		
-		 paramMap.put("id", proid);
-		 paramMap.put("styleSn", styleSn);
-		 paramMap.put("funcSn", funcSn);
-		 paramMap.put("productName", productName);
-		
-		 model.put("proid", proid);
-		 model.put("scStyleSn", styleSn);
-		 model.put("scFuncSn",funcSn);
-		 model.put("ifscfy", ifscfy);
-		
-		  PageInfo page=new PageInfo();
-		  int allPageSize = productAdmService.countProductInfo(paramMap);
-		    page.setTotalRecords(allPageSize);
-		    page.setPerPageInt(pageSize);
-		    page.setCurrentPage(pageNum);
-		    page.pageInfoInvoke(allPageSize, pageSize);
-    /* 	    int startRow = (pageNum - 1) * pageSize;
-	        int offset = pageSize;
-	        PageUtil page = new PageUtil(allPageSize, pageNum, pageSize);*/
-		   
-		    paramMap.put("startNum", page.getStartRecord());
-		    paramMap.put("pageSize", page.getPerPageInt());
-		    
-		    model.put("total", page.getTotalPage());
-		    model.put("current_index",page.getCurrentPage());
-		    List<ProductInfo> productInfoList=null;
-		    List<pano_project_style> proStyleList=null;
-		    List<ProductFeature>  proFuncList=null;
-		    List<ProductClassify> proTypeList=null;
-		    List<PanoVender> proVenderList=null;
-		    
-		    
-			try {
-				productInfoList = productAdmService.listProductInfo(paramMap);
-				proStyleList=productAdmService.listAllProStyle();
-				proFuncList=productAdmService.listAllProFunc();
-				proTypeList=productAdmService.listAllProType();
-				proVenderList = productAdmService.listAllVender();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			
-		   //产品详情显示首个
-		   session.setAttribute("productInfoList", productInfoList);
-			   if(productInfoList!=null&&productInfoList.size()>0){
-			   session.setAttribute("prodtInfo1", productInfoList.get(0));
-		   }
-		   session.setAttribute("proStyleList", proStyleList);
-		   session.setAttribute("proFuncList", proFuncList);
-		   session.setAttribute("proTypeList", proTypeList);
-		   session.setAttribute("proVenderList", proVenderList);
-		   return "/panoadm/productadm/product";
+	public String listproduct(HttpSession session, ModelMap model,
+			String proid, String productName, String styleSn, String funcSn,
+			Integer pageNum, Integer pageSize, String ifscfy) {
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+
+		paramMap.put("id", proid);
+		paramMap.put("styleSn", styleSn);
+		paramMap.put("funcSn", funcSn);
+		paramMap.put("productName", productName);
+
+		model.put("proid", proid);
+		model.put("scStyleSn", styleSn);
+		model.put("scFuncSn", funcSn);
+		model.put("ifscfy", ifscfy);
+
+		PageInfo page = new PageInfo();
+		int allPageSize = productAdmService.countProductInfo(paramMap);
+		page.setTotalRecords(allPageSize);
+		page.setPerPageInt(pageSize);
+		page.setCurrentPage(pageNum);
+		page.pageInfoInvoke(allPageSize, pageSize);
+		/*
+		 * int startRow = (pageNum - 1) * pageSize; int offset = pageSize;
+		 * PageUtil page = new PageUtil(allPageSize, pageNum, pageSize);
+		 */
+
+		paramMap.put("startNum", page.getStartRecord());
+		paramMap.put("pageSize", page.getPerPageInt());
+
+		model.put("total", page.getTotalPage());
+		model.put("current_index", page.getCurrentPage());
+		List<ProductInfo> productInfoList = null;
+		List<pano_project_style> proStyleList = null;
+		List<ProductFeature> proFuncList = null;
+		List<ProductClassify> proTypeList = null;
+		List<PanoVender> proVenderList = null;
+
+		try {
+			productInfoList = productAdmService.listProductInfo(paramMap);
+			proStyleList = productAdmService.listAllProStyle();
+			proFuncList = productAdmService.listAllProFunc();
+			proTypeList = productAdmService.listAllProType();
+			proVenderList = productAdmService.listAllVender();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// 产品详情显示首个
+		session.setAttribute("productInfoList", productInfoList);
+		if (productInfoList != null && productInfoList.size() > 0) {
+			session.setAttribute("prodtInfo1", productInfoList.get(0));
+		}
+		session.setAttribute("proStyleList", proStyleList);
+		session.setAttribute("proFuncList", proFuncList);
+		session.setAttribute("proTypeList", proTypeList);
+		session.setAttribute("proVenderList", proVenderList);
+		return "/panoadm/productadm/product";
 	}
-	
+
 	@RequestMapping("/validateProductId")
-	public void ValidateProductId(HttpServletResponse response,String productId){
-		try{
-			
+	public void ValidateProductId(HttpServletResponse response, String productId) {
+		try {
+
 			int value = productAdmService.ValidateProductId(productId);
-			
+
 			JSONObject json = new JSONObject();
 			json.put("info", value);
 			ajaxOutput(response, json.toString());
-		}catch(IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	@RequestMapping("/preaddpro")
-	public String preaddpro(){
+	public String preaddpro() {
 		return "/panoadm/productadm/addproduct";
 	}
-	
-	//添加
+
+	// 添加
 	@RequestMapping("/addproduct")
-	public String addproduct(Product pro,HttpSession session,HttpServletRequest request,HttpServletResponse response){
+	public String addproduct(Product pro, HttpSession session,
+			HttpServletRequest request, HttpServletResponse response) {
 		pro.setStatus(1);
-		/*PanoLoginModel login=(PanoLoginModel)session.getAttribute("login");
-		long usn=login.getSn();
-		pro.setAdderSn(usn);
-		pro.setUpdaterSn(usn);*/
-		String fullImgSn1=pro.getFullImgSn1();
-		String leftImgSn1=pro.getLeftImgSn1();
-		String downImgSn1=pro.getDownImgSn1();
-		String materialImgSn1=pro.getMaterialImgSn1();
-		String fabricImgSn1=pro.getFabricImgSn1();
+		/*
+		 * PanoLoginModel login=(PanoLoginModel)session.getAttribute("login");
+		 * long usn=login.getSn(); pro.setAdderSn(usn); pro.setUpdaterSn(usn);
+		 */
+		String fullImgSn1 = pro.getFullImgSn1();
+		String leftImgSn1 = pro.getLeftImgSn1();
+		String downImgSn1 = pro.getDownImgSn1();
+		String materialImgSn1 = pro.getMaterialImgSn1();
+		String fabricImgSn1 = pro.getFabricImgSn1();
 		String longImgSn1 = pro.getLongImgSn1();
 		try {
-			if(fullImgSn1!=null&&!"".equals(fullImgSn1)){
-			long fimgsn = EncryptUtil.decode(fullImgSn1);
-			pro.setFullImgSn(fimgsn);
+			if (fullImgSn1 != null && !"".equals(fullImgSn1)) {
+				long fimgsn = EncryptUtil.decode(fullImgSn1);
+				pro.setFullImgSn(fimgsn);
 			}
-			
-			if(leftImgSn1!=null&&!"".equals(leftImgSn1)){
+
+			if (leftImgSn1 != null && !"".equals(leftImgSn1)) {
 				long limgsn = EncryptUtil.decode(leftImgSn1);
 				pro.setLeftImgSn(limgsn);
-				}
-			if(downImgSn1!=null&&!"".equals(downImgSn1)){
+			}
+			if (downImgSn1 != null && !"".equals(downImgSn1)) {
 				long dimgsn = EncryptUtil.decode(downImgSn1);
 				pro.setDownImgSn(dimgsn);
-				}
-			
-			if(materialImgSn1!=null&&!"".equals(materialImgSn1)){
+			}
+
+			if (materialImgSn1 != null && !"".equals(materialImgSn1)) {
 				long mtimgsn = EncryptUtil.decode(materialImgSn1);
 				pro.setMaterialImgSn(mtimgsn);
-				}
-			if(fabricImgSn1!=null&&!"".equals(fabricImgSn1)){
+			}
+			if (fabricImgSn1 != null && !"".equals(fabricImgSn1)) {
 				long fbcimgsn = EncryptUtil.decode(fabricImgSn1);
 				pro.setFabricImgSn(fbcimgsn);
 			}
-			
-			if(longImgSn1!=null&&!"".equals(longImgSn1)){
+
+			if (longImgSn1 != null && !"".equals(longImgSn1)) {
 				long longimgsn = EncryptUtil.decode(longImgSn1);
 				pro.setLongImgSn(longimgsn);
 			}
@@ -173,175 +174,192 @@ public class ProductAdmController extends BaseController{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return redirect("/productadm/listproduct");
 	}
-	
-	//预修改
+
+	// 预修改
 	@RequestMapping("/preupdateproduct")
-	public void preupdateproduct(HttpSession session,HttpServletResponse response,Model model,String productsn){
-		
-		Product prodt=null;
+	public void preupdateproduct(HttpSession session,
+			HttpServletResponse response, Model model, String productsn) {
+
+		Product prodt = null;
 		try {
 			prodt = productAdmService.getProductBySn(productsn);
-			Long fullImgSn=prodt.getFullImgSn();
-			Long leftImgSn=prodt.getLeftImgSn();
-			Long downImgSn=prodt.getDownImgSn();
-			Long materialImgSn=prodt.getMaterialImgSn();
-			Long fabricImgSn=prodt.getFabricImgSn();
-			if(fullImgSn!=null){
-			 String fullImgUrl=fileReadClient.getFile(fullImgSn, FileAttributeEnum.VISIT_ADDR);
-			 prodt.setFullImgUrl(fullImgUrl);
+			Long fullImgSn = prodt.getFullImgSn();
+			Long leftImgSn = prodt.getLeftImgSn();
+			Long downImgSn = prodt.getDownImgSn();
+			Long materialImgSn = prodt.getMaterialImgSn();
+			Long fabricImgSn = prodt.getFabricImgSn();
+			if (fullImgSn != null) {
+				String fullImgUrl = fileReadClient.getFile(fullImgSn,
+						FileAttributeEnum.VISIT_ADDR);
+				prodt.setFullImgUrl(fullImgUrl);
 			}
-		     if(leftImgSn!=null){ 
-			  String leftImgUrl=fileReadClient.getFile(prodt.getLeftImgSn(), FileAttributeEnum.VISIT_ADDR);
-			  prodt.setLeftImgUrl(leftImgUrl);
-		      }
-		     if(downImgSn!=null){
-			  String downImgUrl=fileReadClient.getFile(prodt.getDownImgSn(),FileAttributeEnum.VISIT_ADDR);
-			  prodt.setDownImgUrl(downImgUrl);
-		     }
-		      if(materialImgSn!=null){ 
-		     String materialImgUrl=fileReadClient.getFile(prodt.getMaterialImgSn(), FileAttributeEnum.VISIT_ADDR);
-		     prodt.setMaterialImgUrl(materialImgUrl);
-		      }
-		      if(fabricImgSn!=null){
-		     String fabricImgUrl=fileReadClient.getFile(prodt.getFabricImgSn(), FileAttributeEnum.VISIT_ADDR);
-		     prodt.setFabricImgUrl(fabricImgUrl);
-		      }
-	       
+			if (leftImgSn != null) {
+				String leftImgUrl = fileReadClient.getFile(
+						prodt.getLeftImgSn(), FileAttributeEnum.VISIT_ADDR);
+				prodt.setLeftImgUrl(leftImgUrl);
+			}
+			if (downImgSn != null) {
+				String downImgUrl = fileReadClient.getFile(
+						prodt.getDownImgSn(), FileAttributeEnum.VISIT_ADDR);
+				prodt.setDownImgUrl(downImgUrl);
+			}
+			if (materialImgSn != null) {
+				String materialImgUrl = fileReadClient.getFile(
+						prodt.getMaterialImgSn(), FileAttributeEnum.VISIT_ADDR);
+				prodt.setMaterialImgUrl(materialImgUrl);
+			}
+			if (fabricImgSn != null) {
+				String fabricImgUrl = fileReadClient.getFile(
+						prodt.getFabricImgSn(), FileAttributeEnum.VISIT_ADDR);
+				prodt.setFabricImgUrl(fabricImgUrl);
+			}
+
 		} catch (Exception e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-	  String jsonprodt=	JsonUtils.objectToJson(prodt);
-	  try {
-		this.ajaxOutput(response, jsonprodt);
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	
-		//return this.redirect("/panoadm/productadm/product#product-update");
-	}
-	
-	//修改
-	@RequestMapping("/updateproduct")
-	public String updateproduct(Product pro,HttpSession session,HttpServletRequest request,HttpServletResponse response
-			){
-		/*Product pro=new Product();
-		BigDecimal price=new BigDecimal("843.5");
-		pro.setPrice(price);*/
-    	 pro.setStatus(1);
-		/*PanoLoginModel login=(PanoLoginModel)session.getAttribute("login");
-		long usn=login.getSn();
-		pro.setAdderSn(usn);
-		pro.setUpdaterSn(usn);*/
-		String fullImgSn1=pro.getFullImgSn1();
-		String leftImgSn1=pro.getLeftImgSn1();
-		String downImgSn1=pro.getDownImgSn1();
-		String materialImgSn1=pro.getMaterialImgSn1();
-		String fabricImgSn1=pro.getFabricImgSn1();
-		String longImgSn1 = pro.getLongImgSn1();
-		
+		String jsonprodt = JsonUtils.objectToJson(prodt);
 		try {
-			if(fullImgSn1!=null&&!"".equals(fullImgSn1)){
+			this.ajaxOutput(response, jsonprodt);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// return this.redirect("/panoadm/productadm/product#product-update");
+	}
+
+	// 修改
+	@RequestMapping("/updateproduct")
+	public String updateproduct(Product pro, HttpSession session,
+			HttpServletRequest request, HttpServletResponse response) {
+		/*
+		 * Product pro=new Product(); BigDecimal price=new BigDecimal("843.5");
+		 * pro.setPrice(price);
+		 */
+		pro.setStatus(1);
+		/*
+		 * PanoLoginModel login=(PanoLoginModel)session.getAttribute("login");
+		 * long usn=login.getSn(); pro.setAdderSn(usn); pro.setUpdaterSn(usn);
+		 */
+		String fullImgSn1 = pro.getFullImgSn1();
+		String leftImgSn1 = pro.getLeftImgSn1();
+		String downImgSn1 = pro.getDownImgSn1();
+		String materialImgSn1 = pro.getMaterialImgSn1();
+		String fabricImgSn1 = pro.getFabricImgSn1();
+		String longImgSn1 = pro.getLongImgSn1();
+
+		try {
+			if (fullImgSn1 != null && !"".equals(fullImgSn1)) {
 				long fimgsn = EncryptUtil.decode(fullImgSn1);
 				pro.setFullImgSn(fimgsn);
-				}
-				
-				if(leftImgSn1!=null&&!"".equals(leftImgSn1)){
-					long limgsn = EncryptUtil.decode(leftImgSn1);
-					pro.setLeftImgSn(limgsn);
-					}
-				if(downImgSn1!=null&&!"".equals(downImgSn1)){
-					long dimgsn = EncryptUtil.decode(downImgSn1);
-					pro.setDownImgSn(dimgsn);
-					}
-				
-				if(materialImgSn1!=null&&!"".equals(materialImgSn1)){
-					long mtimgsn = EncryptUtil.decode(materialImgSn1);
-					pro.setMaterialImgSn(mtimgsn);
-					}
-				if(fabricImgSn1!=null&&!"".equals(fabricImgSn1)){
-					long fbcimgsn = EncryptUtil.decode(fabricImgSn1);
-					pro.setFabricImgSn(fbcimgsn);
-				}
-				if(longImgSn1!=null&&!"".equals(longImgSn1)){
-					long longimgsn = EncryptUtil.decode(longImgSn1);
-					pro.setLongImgSn(longimgsn);
-				}
-			boolean bo=productAdmService.updateProduct(pro);
+			}
+
+			if (leftImgSn1 != null && !"".equals(leftImgSn1)) {
+				long limgsn = EncryptUtil.decode(leftImgSn1);
+				pro.setLeftImgSn(limgsn);
+			}
+			if (downImgSn1 != null && !"".equals(downImgSn1)) {
+				long dimgsn = EncryptUtil.decode(downImgSn1);
+				pro.setDownImgSn(dimgsn);
+			}
+
+			if (materialImgSn1 != null && !"".equals(materialImgSn1)) {
+				long mtimgsn = EncryptUtil.decode(materialImgSn1);
+				pro.setMaterialImgSn(mtimgsn);
+			}
+			if (fabricImgSn1 != null && !"".equals(fabricImgSn1)) {
+				long fbcimgsn = EncryptUtil.decode(fabricImgSn1);
+				pro.setFabricImgSn(fbcimgsn);
+			}
+			if (longImgSn1 != null && !"".equals(longImgSn1)) {
+				long longimgsn = EncryptUtil.decode(longImgSn1);
+				pro.setLongImgSn(longimgsn);
+			}
+			boolean bo = productAdmService.updateProduct(pro);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return redirect("/productadm/listproduct");
-		
+
 	}
-	
+
 	@RequestMapping("/deleteproduct")
-	public String deleteproduct(String productsn){
+	public String deleteproduct(String productsn) {
 		try {
 			productAdmService.deleteProduct(productsn);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return redirect("/productadm/listproduct");
 	}
-	 
-	//查看产品详情
+
+	// 查看产品详情
 	@RequestMapping("/getproductdetail")
-	public void getproductdetail(HttpServletResponse response,String productsn){
-		ProductInfo prodtInfo=null;
+	public void getproductdetail(HttpServletResponse response, String productsn) {
+		ProductInfo prodtInfo = null;
 		try {
 			prodtInfo = productAdmService.getProductDetail(productsn);
-			Long fullImgSn=prodtInfo.getFullImgSn();
-			Long leftImgSn=prodtInfo.getLeftImgSn();
-			Long downImgSn=prodtInfo.getDownImgSn();
-			Long materialImgSn=prodtInfo.getMaterialImgSn();
-			Long fabricImgSn=prodtInfo.getFabricImgSn();
+			Long fullImgSn = prodtInfo.getFullImgSn();
+			Long leftImgSn = prodtInfo.getLeftImgSn();
+			Long downImgSn = prodtInfo.getDownImgSn();
+			Long materialImgSn = prodtInfo.getMaterialImgSn();
+			Long fabricImgSn = prodtInfo.getFabricImgSn();
 			Long longImgSn = prodtInfo.getLongImgSn();
-			
-			if(longImgSn!=null){
-				 String longImgUrl=fileReadClient.getFile(longImgSn, FileAttributeEnum.VISIT_ADDR);
-				 prodtInfo.setLongImgUrl(longImgUrl);
-				 prodtInfo.setLongImg(EncryptUtil.encode(longImgSn));
+
+			if (longImgSn != null) {
+				String longImgUrl = fileReadClient.getFile(longImgSn,
+						FileAttributeEnum.VISIT_ADDR);
+				prodtInfo.setLongImgUrl(longImgUrl);
+				prodtInfo.setLongImg(EncryptUtil.encode(longImgSn));
 			}
-			if(fullImgSn!=null){
-			 String fullImgUrl=fileReadClient.getFile(fullImgSn, FileAttributeEnum.VISIT_ADDR);
-			 prodtInfo.setFullImgUrl(fullImgUrl);
-			 prodtInfo.setFullImg(EncryptUtil.encode(fullImgSn));
+			if (fullImgSn != null) {
+				String fullImgUrl = fileReadClient.getFile(fullImgSn,
+						FileAttributeEnum.VISIT_ADDR);
+				prodtInfo.setFullImgUrl(fullImgUrl);
+				prodtInfo.setFullImg(EncryptUtil.encode(fullImgSn));
 			}
-		     if(leftImgSn!=null){ 
-			  String leftImgUrl=fileReadClient.getFile(prodtInfo.getLeftImgSn(), FileAttributeEnum.VISIT_ADDR);
-			  prodtInfo.setLeftImgUrl(leftImgUrl);
-			  prodtInfo.setLeftImg(EncryptUtil.encode(leftImgSn));
-		      }
-		     if(downImgSn!=null){
-			  String downImgUrl=fileReadClient.getFile(prodtInfo.getDownImgSn(),FileAttributeEnum.VISIT_ADDR);
-			  prodtInfo.setDownImgUrl(downImgUrl);
-		     }
-		      if(materialImgSn!=null){ 
-		     String materialImgUrl=fileReadClient.getFile(prodtInfo.getMaterialImgSn(), FileAttributeEnum.VISIT_ADDR);
-		     prodtInfo.setMaterialImgUrl(materialImgUrl);
-		      }
-		      if(fabricImgSn!=null){
-		     String fabricImgUrl=fileReadClient.getFile(prodtInfo.getFabricImgSn(), FileAttributeEnum.VISIT_ADDR);
-		     prodtInfo.setFabricImgUrl(fabricImgUrl);
-		      }
-		   
-		    
+			if (leftImgSn != null) {
+				String leftImgUrl = fileReadClient.getFile(
+						prodtInfo.getLeftImgSn(), FileAttributeEnum.VISIT_ADDR);
+				prodtInfo.setLeftImgUrl(leftImgUrl);
+				prodtInfo.setLeftImg(EncryptUtil.encode(leftImgSn));
+			}
+			if (downImgSn != null) {
+				String downImgUrl = fileReadClient.getFile(
+						prodtInfo.getDownImgSn(), FileAttributeEnum.VISIT_ADDR);
+				prodtInfo.setDownImgUrl(downImgUrl);
+				prodtInfo.setDownImg(EncryptUtil.encode(downImgSn));
+			}
+			if (materialImgSn != null) {
+				String materialImgUrl = fileReadClient.getFile(
+						prodtInfo.getMaterialImgSn(),
+						FileAttributeEnum.VISIT_ADDR);
+				prodtInfo.setMaterialImgUrl(materialImgUrl);
+				prodtInfo.setMaterialImg(EncryptUtil.encode(materialImgSn));
+			}
+			if (fabricImgSn != null) {
+				String fabricImgUrl = fileReadClient.getFile(
+						prodtInfo.getFabricImgSn(),
+						FileAttributeEnum.VISIT_ADDR);
+				prodtInfo.setFabricImgUrl(fabricImgUrl);
+				prodtInfo.setFabricImg(EncryptUtil.encode(fabricImgSn));
+			}
+
 		} catch (Exception e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
-		 String jsonprodt=	JsonUtils.objectToJson(prodtInfo);
-		  try {
+
+		String jsonprodt = JsonUtils.objectToJson(prodtInfo);
+		try {
 			this.ajaxOutput(response, jsonprodt);
 		} catch (IOException e) {
 			// TODO Auto-generated catch blocks
